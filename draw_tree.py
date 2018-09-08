2b4f67e523228cba178e50bf2d38f9e51175e3b0
nozawa <taktaktak0812@gmail.com>
2018-09-08 19:10:29 +0900

    modified

--- draw_tree.py
+++ draw_tree.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from turtle import *
+import turtle
 
 def tree(length):
 	if length > 5:
-		forward(length)
-		right(20)
+		turtle.forward(length)
+		turtle.right(20)
 		tree(length-15)
-		left(40)
+		turtle.left(40)
 		tree(length-15)
-		right(20)
-		backward(length)
+		turtle.right(20)
+		turtle.backward(length)
 
-speed(0)
-color("green")
-left(90)
-backward(150)
+turtle.speed(0)
+turtle.color("green")
+turtle.left(90)
+turtle.backward(150)
 tree(120)
+
 print("hello")
 print("testbranch")
 print("hey!")
